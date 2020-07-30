c1871725d52bd4ad5b5f824976be6db7bac485c2
takenaow48 <takenaow48@gmail.com>
2020-07-30 12:22:27 +0000

    env added

--- config/firebase.js
+++ config/firebase.js
@@ -22,14 +22,14 @@ import 'firebase/auth'
 //   }
 // }
 const config = {
-  apiKey: "AIzaSyDudDeBzA_TDTcBl0ULwNGvsm7EroQn_30",
-  authDomain: "example-nextjs.firebaseapp.com",
-  databaseURL: "https://example-nextjs.firebaseio.com",
-  projectId: "example-nextjs",
-  storageBucket: "example-nextjs.appspot.com",
-  messagingSenderId: "504976824205",
-  appId: "1:504976824205:web:612c6665744646de750fc4",
-  measurementId: "G-8XLBR432BZ"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL:process.env.DATA_BASE_URL,
+  projectId:process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASEREMENT_ID
 }
 const app = firebase.apps.length ? firebase.app() : firebase.initializeApp(config);
 const emailAuth = new firebase.auth.EmailAuthProvider();
